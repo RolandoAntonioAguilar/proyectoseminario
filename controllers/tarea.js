617eaa19148cbd8c2bd87cf35b677c00abaab551
Cristian <claros2328@gmail.com>
2021-03-21 21:02:21 -0600

    Se creó listar una tarea y crear una tarea Signed-off-by: Cristian <claros2328@gmail.com>

--- controllers/tarea.js
+++ controllers/tarea.js
@@ -13,3 +13,33 @@ exports.listTasks = async (req, res, next) => {
     res.status(400).json({ error, message: `Error al listar las tareas` });
   }
 };
+
+exports.listTask = async (req, res, next) => {
+    const { id } = req.params;
+  
+    try {
+      if (!id) res.json({ error, message: `El id de la tarea es necesario` });
+  
+      const tasks = await Task.findById({ _id: id }, { user: 0 });
+      if (!tasks) res.json({ error, message: `Error al listar la tarea` });
+      res.json(tasks);
+    } catch (error) {
+      res.status(400).json({ error, message: `Error al listar la tarea` });
+    }
+  };
+  
+  exports.createTask = async (req, res, next) => {
+    const { title, description } = req.body;
+    try {
+      const newTask = Task({
+        title,
+        description,
+        user: await User.getTokenId(req),
+      });
+      await newTask.save();
+  
+      res.json({ message: `Tarea creada` });
+    } catch (error) {
+      res.status(400).json({ error, message: `Error al crear la tarea` });
+    }
+  };
